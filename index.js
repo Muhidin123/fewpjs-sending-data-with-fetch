bbe9edb7c57cbd510211570a3468969585e3b2fa
Muhidin <muhidinhukic1@gmail.com>
2020-12-11 13:35:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+  let formData = {
+    name: `${name}`,
+    email: `${email}`,
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then((resp) => resp.json())
+    .then((result) => {
+      document.body.innerHTML = result["id"];
+    })
+    .catch((error) => {
+      alert("Bad things! Ragnarők!");
+      document.body.innerHTML = error.message;
+    });
+}
